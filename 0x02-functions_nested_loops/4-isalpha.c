412195e37cfddbb611633aa1544cf342ab36006a
Aphlab1 <shokunbi9999@gmail.com>
2022-09-14 16:53:40 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: the integar value it receives
+ * Description: function returns 1 if the alphabet is a
+ * letter, lowercase, or uppercase.
+ * Return: 1 if true or 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
