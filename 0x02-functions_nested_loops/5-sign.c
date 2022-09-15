6f8de83b50244c4fc790c32576bbbb7ac153e1fa
Aphlab1 <shokunbi9999@gmail.com>
2022-09-14 17:14:56 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - check main
+ * @n: carrier variable
+ * Description: prints the sign of a number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+}
